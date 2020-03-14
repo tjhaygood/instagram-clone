ae17ef3da02dcc65f5f1082556e6f370014b5656
Timothy Haygood <thaygood@ucsc.edu>
2020-03-14 00:21:43 -0700

    IMPORTANT - removed redux devtools line

--- src/store.js
+++ src/store.js
@@ -4,6 +4,7 @@ import allReducer from './reducers';
 const initialState = {}
 
 var myStore = createStore(allReducer, initialState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 
 export default myStore;
